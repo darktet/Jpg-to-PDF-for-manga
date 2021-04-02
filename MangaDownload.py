fe386706a2e1c17f0eff3341a0e84018d3007e85
darktet <santobollove@gmail.com>
2021-04-02 18:38:42 +0600

    Manga Download backup

--- MangaDownload.py
+++ MangaDownload.py
@@ -1,28 +1,56 @@
 import requests
+import jpg_to_pdf
 from bs4 import BeautifulSoup
+from PIL import Image
+import time
 
 
-def rankUrl(animeType, rankNumber=0):
-    calculatRankNumber = ((rankNumber - 1) // 50) * 50
-    if rankNumber <= 50:
-        url = f'https://myanimelist.net/topanime.php?type={animeType}'
-    else:
-        url = f'https://myanimelist.net/topanime.php?type={animeType}&limit={calculatRankNumber}'
-    return url
+
+# class MangSeeImage:
+#     chapter, page = str(0).zfill(4), str(1).zfill(3)
+#     def url(self, url: str):
+#         self.url = url
+
+#     def __init__(self, number: int) -> None:
+#         imageUrl = self.url
+
+def linkGen(chapter: int, page: int):
+    return f'https://temp.compsci88.com/manga/Solo-Leveling/{str(chapter).zfill(4)}-{str(page).zfill(3)}.png'
 
 
-def website(animeType, rankNumber):
-    soup = BeautifulSoup(requests.get(
-        rankUrl(animeType, rankNumber)).text, 'lxml')
-    for trRank in soup.findAll('tr', class_='ranking-list'):
-        rank = trRank.td.span.text
-        animeName = trRank.find('div', class_='di-ib clearfix').a.text
-        imgLink = trRank.findAll('td')[1].a.img['data-src']
-        globalScore = trRank.find(
-            'div', class_='js-top-ranking-score-col di-ib al').span.text
-        animeLink = trRank.find(
-            'a', class_='hoverinfo_trigger fl-l fs14 fw-b')['href']
-        if rank == str(rankNumber):
-            return {'rank': rank, 'animeName': animeName,
-                    'globalScore': globalScore, 'animeLink': animeLink, 'imgLink': imgLink}
+def download(chapter: int):
+    page = 1
+    im1 = Image.open()
+    width, height = im1.size
+    # if imageHight < tmpver:
+    if width == 720:
+        # newImageList.append(imagelist[i])
+        im2 = im1.convert("RGB")
+        # pdf.add_page()
+        # pdf.image(im2.save("__temp.pdf", 'PDF'), 0, 0, 0, 0, "PDF")
+        im2.save(f'data/pdfconvPIL{imgnum}.pdf', 'PDF')
+        # PILimg.append(im2)
+        print(f'Added {imgnum} {imagelist[i][5:]}')
 
+
+
+
+def main():
+    pass
+    # while True:
+    #     try:
+    #         chapter = int(input("Which Cha: "))
+    #         print("Hello Santo", ("1" * 10), type(chapter))
+    #         break
+    #     except ValueError:
+    #         print("Insurt Number")
+    # ii = 'https://temp.compsci88.com/manga/Solo-Leveling/0146-007.png'
+    # y = requests.get('https://temp.compsci88.com/manga/Solo-Leveling/0146-007.png')
+    # print(y.status_code)
+    # x = requests.get('https://temp.compsci88.com/manga/Solo-Leveling/0146-006.png')
+    # print(x.status_code)
+    
+    
+    
+if __name__=="__main__":
+    main()
