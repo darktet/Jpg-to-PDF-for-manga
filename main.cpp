96f3e4c64b88d6545023729db7fbfb685e311302
darktet <santobollove@gmail.com>
2021-04-12 20:52:40 +0600

    Testing mainly removed some unused file

--- main.cpp
+++ main.cpp
@@ -7,15 +7,66 @@
 #include <cmath>
 // #include  <bits/stdc++.h>
 
+// int main()
+// {
+//     std::cout << "Testing" << std::endl;
+//     // (growth rate 'r') xn+1 = r * x (1-x)
+//     long max {3787047968108841941};
+//     unsigned long people_on_earth {max};
 
+//     std::cout << people_on_earth << std::endl;
+//     std::cout << "Short Size: " << sizeof(short) << std::endl;
+//     std::cout << "Int Size: " << sizeof(int) << std::endl;
+
+//     std::cout << std::endl;
+//     return 0;
+// }
+
+// #include <iostream>
+
+// Logistic map
+double f(const double r, const double x)
+{
+    return (r * x * (1 - x));
+}
+
+double rand_unit()
+{
+    return (((double)rand()) / RAND_MAX);
+}
 
 int main()
 {
-    std::cout << "Testing " << std::endl;
-    
+    // std::cout << rand_unit() << std::endl;
+    // std::cout << rand_unit() << std::endl;
+    // std::cout << rand_unit() << std::endl;
+    std::cout << f(2, 0.6) << std::endl;
+    // const double r_min = 3.0;
+    // const double r_max = 4.0;
 
+    // const int num_intervals = 1;
+    // const int max_iterations = 600;
+
+    // srand(time(NULL));
+
+    // double r = r_min;
+    // double x_n;
+
+    // for (int i = 0; i < num_intervals; ++i)
+    // {
+
+    //     r += (r_max - r_min) / num_intervals;
+    //     x_n = f(r, rand_unit());
+
+    //     for (int j = 0; j < max_iterations; ++j)
+    //     {
+    //         x_n = f(r, x_n);
+    //         if (j >= 300)
+    //             std::cout << std::fixed ;
+    //             std::cout << r << ' ' << x_n << " ++ " << std::endl;
+    //     }
+    // }
 
     std::cout << std::endl;
     return 0;
 }
-
--- main.py
+++ main.py
@@ -3,6 +3,39 @@ from PyPDF2 import PdfFileMerger
 import os
 import natsort
 from cachetools import cached, LRUCache
+import sys
+import time
+import itertools
+import threading
+
+
+def load_animation(count: bool):
+
+    load_str = "working on it please wait"
+    ls_len = len(load_str)
+    animation = "|/-\\"
+    anicount = counttime = i = 0
+
+    while count:
+        time.sleep(0.075)
+        load_str_list = list(load_str)
+        x = ord(load_str_list[i])
+        y = 0
+        if x != 32 and x != 46:
+            if x > 90:
+                y = x-32
+            else:
+                y = x + 32
+            load_str_list[i] = chr(y)
+        res = ''
+        for j in range(ls_len):
+            res = res + load_str_list[j]
+        sys.stdout.write("\r"+res + animation[anicount])
+        sys.stdout.flush()
+        load_str = res
+        anicount = (anicount + 1) % 4
+        i = (i + 1) % ls_len
+        counttime = counttime + 1
 
 
 def folderCheck(*dirName: str):
@@ -11,41 +44,68 @@ def folderCheck(*dirName: str):
             os.makedirs(i)
     print(f"Directory created. Insert your images in inputDiractory")
 
+
 @cached(cache=LRUCache(maxsize=2))
 def imageProp(location: str):
     return Image.open(location).convert('RGB')
 
+
 def arrangeFiles(dirName):
     return natsort.natsorted([f"{dirName}/{x}" for x in os.listdir(dirName)])
+
 
 def files(dirNames: str, xPdf: PdfFileMerger):
     i = ii = 0
     for file in arrangeFiles(dirNames):
         ii += 1
         if imageProp(file).width == 720:
-            print(f"{i} file done")
+            # print(f"{i} file done")
             imageProp(file).save(f'{file[:-4]}.pdf', 'PDF')
             xPdf.append(f'{file[:-4]}.pdf')
             os.remove(f'{file[:-4]}.pdf')
             i += 1
-                
+
     print(f"\n\n720: {i}", f"\nNot 720: {ii-i}", f'\nTotal: {ii}')
     # Width == 720 will be convert else not
+
 
 def main():
     outputDir, inputDir = 'outSigPdf', 'inputDiractory'
     if os.path.exists(inputDir) and os.path.exists(outputDir):
+
+        # load_animation(True)
         fullPdf = PdfFileMerger()
         files(inputDir, fullPdf)
         fullPdf.write(f'{outputDir}/JpgtoPDFConvert.pdf')
+        # load_animation(False)
         print(f"Jpg to PDF done check outSigPdf")
-    else: 
+    else:
         folderCheck(inputDir, outputDir)
 
+# here is the animation
+
+
+def animate():
+    for c in itertools.cycle(['|', '/', '-', '\\']):
+        if done:
+            break
+        sys.stdout.write('\rloading ' + c)
+        sys.stdout.flush()
+        time.sleep(0.1)
+    sys.stdout.write('\rDone!     ')
+
+
 if __name__ == '__main__':
-    import time
     start = time.perf_counter()
 
+    done = False
+
+    t = threading.Thread(target=animate)
+    t.start()
+
+    # long process here
     main()
+    time.sleep(10)
+    done = True
 
     print("Time Taken: ", time.perf_counter() - start)
